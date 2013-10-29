53a18e33c7c6c5db74f168525be2c07a8f813da8
Ivan Kafedzhiev <ika@melexis.com>
2013-10-29 11:31:29 +0200

    Added wsh_new_deliveries and removed wip_descrete_jobs

--- viiper-final-lot-shipments/src/main/resources/sql/finallotshipments.sql
+++ viiper-final-lot-shipments/src/main/resources/sql/finallotshipments.sql
@@ -1,15 +1,15 @@
 SELECT DISTINCT wdd.lot_number AS lot_number
 FROM apps.wsh_delivery_details wdd
+  , wsh_new_deliveries  wnd  
   , apps.wsh_delivery_assignments wda
   , apps.hr_organization_units ou
   , apps.mtl_system_items msi
-  , apps.wip_discrete_jobs wdj
 WHERE wda.delivery_detail_id     = wdd.delivery_detail_id
+AND wda.delivery_id              = wnd.delivery_id
 AND ou.organization_id           = wdd.organization_id
 AND msi.inventory_item_id        = wdd.inventory_item_id
 AND msi.organization_id          = wdd.organization_id
 AND msi.item_type                = 'FG'
-AND wdj.lot_number               = wdd.lot_number
 AND wdd.source_header_type_name != 'Internal Order'
 AND wdd.lot_number               = '${in.header.LOTNAME}'
 AND wdd.transaction_id           = ${in.header.TRANSACTION_ID}
