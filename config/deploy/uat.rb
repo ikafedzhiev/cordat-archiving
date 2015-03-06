caa3237a34d7a388be96a1c1a7a060e053c1db91
Alexander Harlampov <alh@melexis.com>
2015-03-06 09:28:38 +0200

    added loading /home/jenkins/.karaf/esb_cap_uat

--- config/deploy/uat.rb
+++ config/deploy/uat.rb
@@ -1,11 +1,3 @@
-set :stage, :uat
-
-set :ssh_options, {
-  auth_methods: ["password"],
-  user: 'smx',
-  password: 'Kon6QuaytUc?',
-  port: 8101,
-  paranoid: false
-}
+require '/home/jenkins/.karaf/esb_cap_uat'
 
 role :karaf, %w{esb-a-uat.colo.elex.be esb-b-uat.colo.elex.be}
