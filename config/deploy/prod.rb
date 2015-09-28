f1154e74154866b2b12899b0c9def4da6e734c8f
Alexander Harlampov <alh@melexis.com>
2015-09-28 12:17:08 +0300

    fix prod target

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,11 +1,3 @@
-set :stage, :prod
-
-set :ssh_options, {
-  auth_methods: ["password"],
-  user: 'smx',
-  password: 'Kon6QuaytUc?',
-  port: 8101,
-  paranoid: false
-}
+require '/home/jenkins/.karaf/esb_cap_prod'
 
 role :karaf, %w{esb-a.colo.elex.be esb-b.colo.elex.be}
